e863e0f0adeeddc2a0e31dc7afc4ef2950ee1c10
darckronoz <crispipe14@gmail.com>
2023-04-15 23:36:00 -0500

    test deploy 2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
-import React from 'react'
-import ReactDom from 'react-dom/client'
+import React from "react";
+import ReactDom from "react-dom/client"; 
 
-const root = ReactDom.createRoot(document.getElementById('root'));
-root.render(<h1>--Budgify initial html--</h1>)
+const root = ReactDom.createRoot(document.getElementById("root"));
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Título de la página</h1>
+      </header>
+      <main>
+        <p>Contenido de la página</p>
+      </main>
+      <footer>
+        <p>Derechos de autor © 2023 - Nombre de la empresa</p>
+      </footer>
+    </div>
+  );
+}
+
+
+root.render(
+  <>
+    <App/>
+  </>
+);
