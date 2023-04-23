8a123c944a9c47ffdddfbbc4f1fe1adbe85c1087
unknown <leonardovalde16@gmail.com>
2023-04-22 22:45:48 -0500

    add register front

--- src/Register.js
+++ src/Register.js
@@ -1,13 +1,31 @@
 import React from "react";
+import UserForm from "./components/UserForm";
+import CheckTerm from "./components/CheckTerm";
 import Header from "./components/Header";
 
 function Register() {
-
   return (
     <div>
       <Header/>
-      <hr />
-      
+      <h1 id="RegisterTitle">Sign up for free on <br/>Budgify</h1>
+      <div>
+        <UserForm header={"Whats your username?"} />
+        <UserForm header={"Whats your email address?"} />
+        <UserForm header={"Confirm your email address?"} />
+        <UserForm header={"Create a password"} />
+        <UserForm header={"Confirm your password"} />
+        <br/>
+        <CheckTerm terms={"I want to receive marketing messages from Budgify"}/>
+        <br/>
+        <CheckTerm terms={"Sharing my registration data with Budgify's content providers for marketing purposes."}/>
+        <br/>
+        <p id="termsRegister">
+            By clicking Sign Up, you agree to Budgify's <span style={{ color: "#BC00FF" }}>Terms and Conditions</span> and <span style={{ color: "#BC00FF" }}>Privacy Policy.</span>
+        </p>
+        <button id="roundButton">REGISTER</button> 
+        <br/>
+        <p id="lastLogin">Already have an account? <span style={{ color: "#BC00FF" }}>Log in.</span></p>
+      </div>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,10 @@ function Header() {
     <div class="LoginAndRegisterHeader">
       <img
         src="https://firebasestorage.googleapis.com/v0/b/budgify-ed7a9.appspot.com/o/LogoBlack.png?alt=media&token=203a5607-2e84-47bd-8417-49e7c5c93fd5"
-        id="headerImage" alt=""/>
+        id="headerImage"
+        alt=""
+      />
+      <hr />
     </div>
   );
 }
