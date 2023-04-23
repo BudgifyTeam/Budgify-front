d92f5dc09c3e7f9badd6a3a1b0a9a1fe2dd6d397
unknown <leonardovalde16@gmail.com>
2023-04-22 20:42:58 -0500

    add routing to register

---
+++ src/Register.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/Header";
+
+function Register() {
+
+  return (
+    <div>
+      <Header/>
+      <hr />
+      
+    </div>
+  );
+}
+
+export default Register;
