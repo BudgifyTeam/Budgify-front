d1e72e2b74a120aa0a7498517a4ceb97d5235e3b
darckronoz <crispipe14@gmail.com>
2023-05-01 18:01:12 -0500

    login and remember flow added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDom from "react-dom/client";
 import Login from "./Login"
 import Register from "./Register"
 import App from "./Home"
+import Dashboard from "./Dashboard";
 import { BrowserRouter as Router, Routes ,Route } from 'react-router-dom';
 
 const root = ReactDom.createRoot(document.getElementById("root"));
@@ -12,6 +13,7 @@ root.render(
     <Router>
       <Routes>
         <Route exact path="/" element={<App/>}/>
+        <Route exact path="/dashboard" element={<Dashboard/>}/>
         <Route exact path="/register" element={<Register/>}/>
         <Route exact path="/login" element={<Login/>}/>
       </Routes>
