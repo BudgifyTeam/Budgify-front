37b3375360769d59f54c6aabb1383d16a82d8c70
SebastianLopez03 <127448028+SebastianLopez03@users.noreply.github.com>
2023-05-02 21:48:50 -0500

    Fixed the visual bug of the checkboxes

--- src/components/FormsComponents.js
+++ src/components/FormsComponents.js
@@ -4,10 +4,12 @@ import React from "react";
 export function CheckTerm(props) {
   return (
     <div style={{marginLeft: "5%", display: "flex", alignItems: "flex-end", flexWrap: "nowrap" }}>
-      <div>
-        <input type="checkbox" id="myCheckbox" checked={props.checked} onChange={props.onChange}/>
+      <div >
+        <tr>
+          <td><input type="checkbox" id="myCheckbox" checked={props.checked} onChange={props.onChange}/></td>
+          <td id="Term">{props.terms}</td>
+        </tr>
       </div>
-      <div id="Term">{props.terms}</div>
     </div>
   );
 }
