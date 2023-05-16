67db07e503b751e1cd66a6e1f3955fdc6c441e60
unknown <leonardovalde16@gmail.com>
2023-05-16 00:29:11 -0500

    add login request, and basic model to other pages

--- src/pages/Wallets.js
+++ src/pages/Wallets.js
@@ -1,16 +1,11 @@
-import React, {useState} from "react";
+import React from "react";
 import { Footer, Header } from "../components/AppComponents";
 
 function Wallets(){
-    const [budgetValue, setBudgetValue] = useState(() => {
-        const storedValue = localStorage.getItem("budgetValue");
-        return storedValue ? parseInt(storedValue) : 0;
-      });
-      const [image, setImage] = useState("https://firebasestorage.googleapis.com/v0/b/budgify-ed7a9.appspot.com/o/userimage.jpg?alt=media&token=df5dc86a-c48e-4786-9501-565b2ad15134");
-    
+  
       return (
         <div className="dashboardContainer">
-          <Header budgetValue={budgetValue} image={image} />
+          <Header/>
           <div>
             <>Wallets</>
           </div>
