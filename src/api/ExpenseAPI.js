d6751991e3e4b1857a2250cf04ef193ec7dc6faa
unknown <leonardovalde16@gmail.com>
2023-05-27 16:05:04 -0500

    add alerts to expense and incomes

--- src/api/ExpenseAPI.js
+++ src/api/ExpenseAPI.js
@@ -1,7 +1,14 @@
 import url_back from "./config";
-export async function MakeExpenseRequest(walletId, inputValue, selectedDate, pocket_id, category_id) {
-  console.log("EXPENSE");  
-  const baseUrl = url_back + "Expense/CreateExpense";
+export async function MakeExpenseRequest(
+  walletId,
+  inputValue,
+  selectedDate,
+  pocket_id,
+  category_id
+) {
+  console.log("EXPENSE");
+  if (inputValue.length !== 0) {
+    const baseUrl = url_back + "Expense/CreateExpense";
     let date = new Date();
     var queryParams = new URLSearchParams({
       userid: parseInt(localStorage.getItem("userId")),
@@ -15,10 +22,10 @@ export async function MakeExpenseRequest(walletId, inputValue, selectedDate, poc
         ":00Z",
       wallet_id: walletId,
       pocket_id: pocket_id,
-      category_id: category_id
+      category_id: category_id,
     });
     const url = `${baseUrl}?${queryParams.toString()}`;
-  
+
     try {
       const response = await fetch(url, {
         method: "POST",
@@ -26,13 +33,12 @@ export async function MakeExpenseRequest(walletId, inputValue, selectedDate, poc
           Accept: "text/plain",
         },
       });
-  
+
       if (!response.ok) {
         throw new Error(
           "Error en la solicitud. Código de respuesta: " + response.status
         );
       }
-  
       const responseData = await response.json();
       const { newBudget } = responseData; // Extract the newBudget value from the response
       console.log(newBudget);
@@ -43,4 +49,10 @@ export async function MakeExpenseRequest(walletId, inputValue, selectedDate, poc
       console.error(error);
       throw error;
     }
+  }else{
+    return {
+      message: "El Valor es nulo o cero",
+      code: false
+    };
   }
+}
--- src/components/IncomeAndExpenseComponents.js
+++ src/components/IncomeAndExpenseComponents.js
@@ -1,13 +1,17 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import "./IncomeAndExpenseComponents.css";
-import { Link } from "react-router-dom";
 
 export function IAEValueInput(props) {
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
   const handleInputChange = (event) => {
     const value = event.target.value;
-    setInputValue(value);
-    props.onInputChange(value); // Llama a la función de actualización en el componente padre
+    const numericValue = value.replace(/[^0-9]/g, ""); // Remueve caracteres no numéricos
+    setInputValue(numericValue);
+    props.onInputChange(numericValue); // Llama a la función de actualización en el componente padre
+    if (numericValue === "") {
+      setInputValue(""); // Establece el valor vacío si no hay números ingresados
+      props.onInputChange(""); // Llama a la función de actualización en el componente padre con el valor vacío
+    }
   };
   return (
     <>
@@ -21,7 +25,6 @@ export function IAEValueInput(props) {
     </>
   );
 }
-
 
 export function CategorySelector(props) {
   console.log(props.categories);
@@ -49,7 +52,7 @@ export function WalletSelector(props) {
     <>
       <h2>Wallet</h2>
       <select
-        id="walletSelector"
+        id="categorySelector"
         className="form-select"
         onChange={handleWalletChange}
       >
@@ -73,7 +76,7 @@ export function PocketSelector(props) {
     <>
       <h2>Pocket</h2>
       <select
-        id="pocketSelector"
+        id="categorySelector"
         className="form-select"
         onChange={handleWalletChange}
       >
@@ -87,9 +90,11 @@ export function PocketSelector(props) {
     </>
   );
 }
-
 export function DateSelector(props) {
-  const [selectedDate, setSelectedDate] = useState(new Date().toISOString().split("T")[0]);
+  const [selectedDate, setSelectedDate] = useState(
+    new Date().toISOString().split("T")[0]
+  );
+  const [selectedButtonIndex, setSelectedButtonIndex] = useState(null);
 
   let tomorrow = new Date();
   tomorrow.setDate(tomorrow.getDate() + 1);
@@ -100,57 +105,58 @@ export function DateSelector(props) {
   const handleDateChange = (event) => {
     const date = event.target.value;
     setSelectedDate(date);
+    setSelectedButtonIndex(null); // Reinicia el índice del botón seleccionado
     props.onDateChange(date); // Llama a la función de actualización en el componente padre
   };
 
-  const handleTomorrowDateButtonClick = () => {
-    const date = new Date();
-    date.setDate(date.getDate() + 1);
-    setSelectedDate(date.toISOString().split("T")[0]);
-    props.onDateChange(date.toISOString().split("T")[0]); // Llama a la función de actualización en el componente padre
-  };
-
-  const handleYesterdayDateButtonClick = () => {
-    const date = new Date();
-    date.setDate(date.getDate() - 1);
-    setSelectedDate(date.toISOString().split("T")[0]);
-    props.onDateChange(date.toISOString().split("T")[0]); // Llama a la función de actualización en el componente padre
+  const handleButtonClick = (index, date) => {
+    setSelectedDate(date);
+    setSelectedButtonIndex(index); // Actualiza el índice del botón seleccionado
+    props.onDateChange(date); // Llama a la función de actualización en el componente padre
   };
 
   return (
     <table>
-      <tr>
-        <td colSpan="2">
-          <input
-            type="date"
-            value={selectedDate}
-            onChange={handleDateChange}
-            id="dateSelector"
-          />
-        </td>
-      </tr>
-      <tr id="premadeButtonsContiner">
-        <td>
-          <button
-            onClick={handleYesterdayDateButtonClick}
-            id="premadeDateButton"
-          >
-            {yesterday.toISOString().split("-")[1]}/
-            {yesterday.toISOString().split("-")[2].split("T")[0]} <br />
-            Yesterday
-          </button>
-        </td>
-        <td>
-          <button
-            onClick={handleTomorrowDateButtonClick}
-            id="premadeDateButton"
-          >
-            {tomorrow.toISOString().split("-")[1]}/
-            {tomorrow.toISOString().split("-")[2].split("T")[0]} <br />
-            Tomorrow
-          </button>
-        </td>
-      </tr>
+      <tbody>
+        <tr>
+          <td colSpan="2">
+            <input
+              type="date"
+              value={selectedDate}
+              onChange={handleDateChange}
+              id="dateSelector"
+            />
+          </td>
+        </tr>
+        <tr id="premadeButtonsContiner">
+          <td>
+            <button
+              onClick={() =>
+                handleButtonClick(0, yesterday.toISOString().split("T")[0])
+              }
+              id="premadeDateButton"
+              style={{ borderWidth: selectedButtonIndex === 0 ? "2px" : "1px" }} // Resalta el botón si su índice coincide con selectedButtonIndex
+            >
+              {yesterday.toISOString().split("-")[1]}/
+              {yesterday.toISOString().split("-")[2].split("T")[0]} <br />
+              Yesterday
+            </button>
+          </td>
+          <td>
+            <button
+              onClick={() =>
+                handleButtonClick(1, tomorrow.toISOString().split("T")[0])
+              }
+              id="premadeDateButton"
+              style={{ borderWidth: selectedButtonIndex === 1 ? "2px" : "1px" }} // Resalta el botón si su índice coincide con selectedButtonIndex
+            >
+              {tomorrow.toISOString().split("-")[1]}/
+              {tomorrow.toISOString().split("-")[2].split("T")[0]} <br />
+              Tomorrow
+            </button>
+          </td>
+        </tr>
+      </tbody>
     </table>
   );
 }
@@ -159,11 +165,10 @@ export function OperationIncomeMenu(props) {
   const handleClick = () => {
     props.onClick(props.option);
   };
+
   return (
     <div id="incomeOrExpenseMenu">
-      <Link to="/dashboard">
-        <OperationButton option="Add Income" onClick={handleClick} />
-      </Link>
+      <OperationButton option="Add Income" onClick={handleClick} />
     </div>
   );
 }
@@ -174,9 +179,7 @@ export function OperationExpenseMenu(props) {
   };
   return (
     <div id="incomeOrExpenseMenu">
-      <Link to="/dashboard">
         <OperationButton option="Add Expense" onClick={handleClick} />
-      </Link>
     </div>
   );
 }
